3293674b7c290bcfac4815fb2daadeb9419f528f
Askar Nasyrov <ACKAP.mail@bk.ru>
2025-01-03 18:27:05 +0300

    ВDone

--- homework-classes.py
+++ homework-classes.py
@@ -1,4 +1,5 @@
 class Person:
+
     def __init__(self, name, surname, gender = None):
         self.name = name
         self.surname = surname
@@ -6,17 +7,90 @@ class Person:
 
 
 class Student(Person):
+
     def __init__(self, name, surname, gender):
         super().__init__(name, surname, gender)
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if (isinstance(lecturer, Lecturer)
+                and course in self.courses_in_progress and course in lecturer.courses_attached):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'You cannot rate lecturer for that course.'
+
+    def average_grades(self):
+        rating = 0
+        length = 0
+        if self.grades is None:
+            return 'No grades.'
+        else:
+            for value in self.grades.values():
+                rating += sum(value)
+                length += len(value)
+            rating = round(rating / length, 2)
+            return rating
+
+    def __str__(self):
+        return (f'Имя: {self.name}'
+                f'\nФамилия: {self.surname}'
+                f'\nСредняя оценка за домашние задания: {self.average_grades()}'
+                f'\nКурсы в процессе изучения: {self.courses_in_progress}'
+                f'\nЗавершенные курсы: {self.finished_courses}'
+                f'\n')
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return 'Can not compare'
+        return self.average_grades() < other.average_grades()
+
 
 class Mentor(Person):
+
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def average_grades(self):
+        rating = 0
+        length = 0
+        if self.grades is None:
+            return 'No grades.'
+        else:
+            for value in self.grades.values():
+                rating += sum(value)
+                length += len(value)
+            rating = round(rating / length, 2)
+            return rating
+
+    def __str__(self):
+        return (f'Имя: {self.name}'
+                f'\nФамилия: {self.surname}'
+                f'\nСредняя оценка за лекции: {self.average_grades()}'
+                f'\n')
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return 'Can not compare'
+        return self.average_grades() < other.average_grades()
+
+
+class Reviewer(Mentor):
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -25,27 +99,59 @@ class Mentor(Person):
             else:
                 student.grades[course] = [grade]
         else:
-            return 'Ошибка'
+            return 'You cannot rate student for that course.'
+
+    def __str__(self):
+        return (f'Имя: {self.name}'
+                f'\nФамилия: {self.surname}'
+                f'\n')
 
 
-class Lecture(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
+# Студенты
+student_1 = Student('Аскар', 'Насыров', 'М')
+student_1.courses_in_progress += ['Python']
+student_1.finished_courses += ["Введение в программирование"]
 
+student_2 = Student('Анастасия', 'Сина', 'Ж')
+student_2.courses_in_progress += ['Python']
+student_2.finished_courses += ["Введение в программирование"]
 
-class Review(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
+# Лекторы
+lecturer_1 = Lecturer('Андрей', 'Быков')
+lecturer_1.courses_attached += ['Python']
 
+lecturer_2 = Lecturer('Ирина', 'Попова')
+lecturer_2.courses_attached += ['Python']
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+# Проверяющие
+reviewer_1 = Reviewer('Сергей', 'Сергеев')
+reviewer_1.courses_attached += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+reviewer_2 = Reviewer('Ольга', 'Петровна')
+reviewer_2.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+# Оценки студентам
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 9)
+reviewer_1.rate_hw(student_1, 'Python', 7)
 
-print(best_student.grades)
+reviewer_2.rate_hw(student_2, 'Python', 10)
+reviewer_2.rate_hw(student_2, 'Python', 9)
+reviewer_2.rate_hw(student_2, 'Python', 9)
+
+# Оценки лекторам
+student_1.rate_lecturer(lecturer_1, 'Python', 10)
+student_1.rate_lecturer(lecturer_1, 'Python', 8)
+student_1.rate_lecturer(lecturer_1, 'Python', 6)
+
+student_2.rate_lecturer(lecturer_2, 'Python', 10)
+student_2.rate_lecturer(lecturer_2, 'Python', 6)
+student_2.rate_lecturer(lecturer_2, 'Python', 6)
+
+print(student_1)
+print(student_2)
+print(lecturer_1)
+print(lecturer_2)
+print(reviewer_1)
+print(reviewer_2)
+print(student_1>student_2)
